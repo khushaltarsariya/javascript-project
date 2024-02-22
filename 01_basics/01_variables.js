db1aa950390de2ec07ec237dedac6eabd8659ca2
khushaltarsariya <71313976+khushaltarsariya@users.noreply.github.com>
2024-02-22 12:33:04 +0000

    adding the datatype

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,8 +3,15 @@ const account_Id = 14435
 
 let account_Email = "xyz@email.com"
 
+//prefer not to use var because issue in the block scope and functional scope
 var accountPassword = "1234"
 
+//you are able to declare the variable in the javascript without declaring the let or const keyword 
 accountCity = "Surat"
-//account_Id = 2
-console.log(account_Id);
+accountPassword= "212121"
+account_Email="abc@.com"
+
+//console.log(account_Id);
+
+//we can use this thing for the output in the table formate
+console.table([accountCity,accountPassword,account_Email]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+//Datatype in the javascript
+
+"use strict"; //this is treated as the newer version of the javascript 
+
+let name = "khushal" //string 
+let age = 23 //int 
+let value = true //boolean 
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false 
+// null => this is also standalon value and also a datatype 
+// undefine => when you assign nothing to the variable then it is undefine 
+// symbol => for finding the uniqueness the symbole is use 
+
+
+//object => 
+//
+console.log(typeof null); // this will give the object type 
+console.log(typeof undefine);// undefine type 
