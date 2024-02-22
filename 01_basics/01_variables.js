3dba9d14c6bc2322997431f59053e0bcf6585f48
khushaltarsariya <71313976+khushaltarsariya@users.noreply.github.com>
2024-02-22 11:29:09 +0000

    initial

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+//here we are decalring the constant variable we are not able to change this variable
+const account_Id = 14435
+
+let account_Email = "xyz@email.com"
+
+var accountPassword = "1234"
+
+accountCity = "Surat"
+//account_Id = 2
+console.log(account_Id);
